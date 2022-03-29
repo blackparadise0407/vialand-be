8d218bc05f2ba1f484dae389135674ed4e9712d9
blackparadise0407 <blackparadise0407@gmail.com>
2022-03-29 22:44:05 +0700

    Remove cors origins

--- src/index.js
+++ src/index.js
@@ -15,11 +15,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(
-  cors({
-    origin: '*',
-  }),
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
