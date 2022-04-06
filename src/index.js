2d2256c595dc261cbe6eaf57686b0bf82c2973eb
Khoa Pham <khoa.pham@publishone.nl>
2022-04-06 10:29:15 +0700

    Fix job only run in production

--- src/index.js
+++ src/index.js
@@ -21,12 +21,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-schedule.scheduleJob('*/14 * * * *', function () {
-  fetch('https://vialand.herokuapp.com/ping')
-    .then((res) => res.text())
-    .then((data) => console.log(data))
-    .catch((e) => console.log(e));
-});
+if (process.env.NODE_ENV !== 'development') {
+  schedule.scheduleJob('*/14 * * * *', function () {
+    fetch('https://vialand.herokuapp.com/ping')
+      .then((res) => res.text())
+      .then((data) => console.log(data))
+      .catch((e) => console.log(e));
+  });
+}
 
 app.get('/ping', (req, res) => {
   res.send('pong').status(200);
