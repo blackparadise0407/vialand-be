1ace020d644a1fbf94a9d41464ba05e313582b41
blackparadise0407 <blackparadise0407@gmail.com>
2022-05-17 22:33:52 +0700

    Fix

--- src/index.js
+++ src/index.js
@@ -73,6 +73,7 @@ app.get('/renew', auth, async (_, res, next) => {
         scope: 'https://www.googleapis.com/auth/drive',
         response_type: 'code',
         access_type: 'offline',
+        prompt: 'consent',
       });
 
     const resp = new AppResponse(link);
