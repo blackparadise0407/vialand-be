a0a78c273262ec5acb21ad3cfa32e40237faea06
blackparadise0407 <blackparadise0407@gmail.com>
2022-06-06 17:38:15 +0700

    Remove log

--- src/routes/google.js
+++ src/routes/google.js
@@ -38,9 +38,6 @@ router.get(
     if (data.error) {
       throw data;
     }
-
-    console.log(data);
-
     await db.collection('tokens').add({
       value: data.refresh_token,
       expiredAt: dayjs().add(6, 'day').unix(),
